7f0b88ba32eaeb70c3bbc0cfbe1b418d679de237
nixmojo11 <nikhilnairdesigns@gmail.com>
2022-01-06 11:28:38 +0530

    JavaScript
    Responsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+console.log(" Liked my works? Contact me for any UI design related work.");
+function menuToggle() {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+    x.className += "responsive";
+} else {
+    x.className = "navtoggle";
+}
+}

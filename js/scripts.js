0781cd14ddf807abff63850ce2edafa8f89fbbde
nixmojo11 <nikhilnairdesigns@gmail.com>
2022-01-06 18:49:47 +0530

    Dropdown menu
    Making it responsive to all screen sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 console.log(" Liked my works? Contact me for any UI design related work.");
+
 function menuToggle() {
-    var x = document.getElementById("myNavtoggle");
-    if (x.className === "navtoggle") {
-    x.className += "responsive";
+    var x = document.getElementById('myNavtoggle')
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive'
 } else {
-    x.className = "navtoggle";
+    x.className = 'navtoggle'
 }
 }
